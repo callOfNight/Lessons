5153d5692ca25cb80b433ec00615110f85b4f539
callOfNight <callOfNight@users.noreply.github.com>
2026-05-24 01:49:16 +0300

    Quartz sync: May 24, 2026, 1:49 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "callOfNight.github.io/Lessons",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
